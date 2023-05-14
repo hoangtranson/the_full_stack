30ad74125ab9f217104e99413cd6926c10478a82
hoangtranson <son.hoang01@gmail.com>
2023-05-14 23:15:37 +0700

    add postgresql

--- the_full_stack/settings.py
+++ the_full_stack/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'the_full_stack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'the_full_stack_dev',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
